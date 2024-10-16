83307b2478fabf04d9ae522cb8433add4e69f70a
cohamil <coohamil@ucsc.edu>
2024-10-16 16:24:06 -0700

    Step 2: added drawing and clear button

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,52 @@ if (!ctx) {
   throw new Error("Failed to get 2d context from canvas");
 }
 
+// Create global variables
+const cursor = { isDrawing: false, x: 0, y: 0 };
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+canvas.addEventListener("mousedown", (e) => {
+    cursor.isDrawing = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  });
+  
+canvas.addEventListener("mousemove", (e) => {
+if (cursor.isDrawing) {
+    drawLine(ctx, cursor.x, cursor.y, e.offsetX, e.offsetY);
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+}
+});
+  
+addEventListener("mouseup", (e) => {
+if (cursor.isDrawing) {
+    drawLine(ctx, cursor.x, cursor.y, e.offsetX, e.offsetY);
+    cursor.x = 0;
+    cursor.y = 0;
+    cursor.isDrawing = false;
+}
+});
+
+// Create clear button
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+
+// ----------------------------- Functions -----------------------------
+
+
+function drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}

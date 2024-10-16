fcbc4579e03012be43e423067fffbfad93494685
cohamil <coohamil@ucsc.edu>
2024-10-16 16:04:02 -0700

    Step 1: creating/setting up canvas ui

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
-const APP_NAME = "Greetings";
+const APP_NAME = "Knockoff MS Paint";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
+
+// create canvas
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+app.append(canvas);
+const ctx = canvas.getContext("2d");
+if (!ctx) {
+  throw new Error("Failed to get 2d context from canvas");
+}
+
